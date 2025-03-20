951acbaa89103afd84ed1f3b719bfed8a2db371e
PiyushPawar06 <piyushpawar274@gmail.com>
2025-03-20 23:19:24 +0530

    Section 3 data types- int and float

---
+++ 3_python_basics/python_datatypes.py
@@ -0,0 +1,15 @@
+#Fundametal Data Types
+int
+str
+bool
+dict
+float
+set
+tuple
+list
+
+#classes -> custom data types made by user 
+
+#specialized data types -> modules, packages
+
+None # its doesnt have any value
---
+++ 3_python_basics/python_numbers.py
@@ -0,0 +1,13 @@
+# data types and maths
+#int and float
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+#to know the type of class ie. int for the below case
+print(type(2 + 4))
+
+print(2//4) # floor division the values after decimal are not printed
+print(2**3) # ** means raise to the power of 
+print(4%2) # modulo will give the reminder
