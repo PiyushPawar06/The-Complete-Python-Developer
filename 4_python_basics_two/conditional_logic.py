a306e0ec98659fa3f1c10fa1a9d0b72baf70e5d8
PiyushPawar06 <piyushpawar274@gmail.com>
2025-04-13 17:00:19 +0530

    Day 4 Pythons Basics - conditional flow

---
+++ 4_python_basics_two/conditional_logic.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licenced = False
+
+if is_licenced:
+    print("You are eligible")
+elif is_old:
+    print("You are eligible but not licenced")
+else:
+    print("You are underage")
